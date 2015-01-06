ad4d0e3c85790de6b50d5908fe7534d234dbed7d
Gabriel Morais <gabmla19@gmail.com>
2015-01-06 19:28:08 -0300

    Opiniao

--- ProjetoP2/src/classes/Opiniao.java
+++ ProjetoP2/src/classes/Opiniao.java
@@ -1,5 +1,24 @@
 package classes;
 
 public class Opiniao {
-	// A ser feito
+	private String comentario;
+	private int nota;
+	
+	public Opiniao(String comentario, int nota){
+		this.comentario = comentario;
+		this.nota = nota;
+	}
+	
+	public String getComentario(){
+		return this.comentario;
+	}
+	
+	public int getNota(){
+		return this.nota;
+	}
+	
+	@Override
+	public String toString() {
+		return "Comentario: " + getComentario() + "\nNota: " + getNota();
+	}
 }
