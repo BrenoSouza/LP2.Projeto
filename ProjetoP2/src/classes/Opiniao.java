43a2a69d7e9fd80c1704e6fe1c86285a8dae973f
Gabriel Dantas <gabriel.dantas@ccc.ufcg.edu.br>
2015-01-06 20:49:04 -0300

    Adição de mais uma condição na excessão.

--- ProjetoP2/src/classes/Opiniao.java
+++ ProjetoP2/src/classes/Opiniao.java
@@ -5,8 +5,8 @@ public class Opiniao {
 	private int nota;
 	
 	public Opiniao(String comentario, int nota) throws Exception{
-		if (comentario.length() > 140){
-			throw new Exception("O comentario deve possuir no maximo 140 caracteres.");
+		if (comentario.length() > 140 || comentario.isEmpty() || comentario == null){
+			throw new Exception("O comentario não pode ser vazio e deve possuir no maximo 140 caracteres.");
 		}
 		if (nota < 0 || nota > 5){
 			throw new Exception("A nota deve ser entre 0 e 5.");
