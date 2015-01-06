d5a4083889e0b59ac26c4e710d3566bdc3665f91
GabrielDantas <gabriel.dantas@ccc.ufcg.edu.br>
2015-01-06 18:32:09 -0300

    Delete OlaMundo.java

--- ProjetoP2/src/helloWorld/OlaMundo.java
+++
@@ -1,9 +0,0 @@
-package helloWorld;
-
-public class OlaMundo {
-public static void main(String[] args) {
-	System.out.println("Ola ufcg");
-	System.out.println("La vamos nos!");
-	System.out.println("BONDE DA PROGRAMAÇÃO SEM FREIO");
-}
-}
