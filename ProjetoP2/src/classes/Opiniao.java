ef04f15b35642722b2cbedd0f90fe46a6b65251d
Gabriel Morais <gabmla19@gmail.com>
2015-01-06 19:39:36 -0300

    Exception no opiniao

--- ProjetoP2/src/classes/Opiniao.java
+++ ProjetoP2/src/classes/Opiniao.java
@@ -4,7 +4,13 @@ public class Opiniao {
 	private String comentario;
 	private int nota;
 	
-	public Opiniao(String comentario, int nota){
+	public Opiniao(String comentario, int nota) throws Exception{
+		if (comentario.length() > 140){
+			throw new Exception("O comentario deve possuir no maximo 140 caracteres.");
+		}
+		if (nota < 0 || nota > 5){
+			throw new Exception("A nota deve ser entre 0 e 5.");
+		}
 		this.comentario = comentario;
 		this.nota = nota;
 	}
