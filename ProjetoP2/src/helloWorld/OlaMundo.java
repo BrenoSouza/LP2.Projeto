b47fe0293107cac568f123af5d9678fbb565e76d
Gabriel Dantas <gabriel.dantas@ccc.ufcg.edu.br>
2015-01-06 17:07:48 -0300

    Agora vai

--- ProjetoP2/src/helloWorld/OlaMundo.java
+++ ProjetoP2/src/helloWorld/OlaMundo.java
@@ -4,5 +4,6 @@ public class OlaMundo {
 public static void main(String[] args) {
 	System.out.println("Ola ufcg");
 	System.out.println("La vamos nos!");
+	System.out.println("BONDE DA PROGRAMAÇÃO SEM FREIO");
 }
 }
