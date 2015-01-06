1cd31a874b6854bb2a21bdbfc1865f03fd4a08b8
Gabriel Dantas <gabriel.dantas@ccc.ufcg.edu.br>
2015-01-06 19:59:10 -0300

    toString da classe Hospede

--- ProjetoP2/src/classes/Hospede.java
+++ ProjetoP2/src/classes/Hospede.java
@@ -7,37 +7,73 @@ public class Hospede {
 	private String nome, cpf;
 	private Calendar dataNascimento;
 	private Opiniao opiniao;
-	
+	/**
+	 * Contrutor da classe Hospede.
+	 * @param nome
+	 * O nome do hóspede.
+	 * @param cpf
+	 * O CPF do hóspede.
+	 * @param dataNascimento
+	 * Um objeto Calendar contendo a data de nascimento do hóspede.
+	 * @throws Exception
+	 * Joga uma excessão caso qualquer um dos atributos estiverem em formato inválido.
+	 */
 	public Hospede(String nome, String cpf, Calendar dataNascimento) throws Exception{
 		if (nome == null|| nome.isEmpty() || cpf == null || cpf.isEmpty() || dataNascimento == null){
-			throw new Exception ("Dados inválidos. Tenta novamente");
+			throw new Exception ("Dados inválidos. Tente novamente");
 		}
 		this.nome = nome;
 		this.cpf = cpf;
 		this.dataNascimento = dataNascimento;
 		this.opiniao = null;
 	}
-
+	/**
+	 * Getter do nome do hóspede.
+	 * @return
+	 * O nome do hóspede.
+	 */
 	public String getNome() {
 		return nome;
 	}
-
+	/**
+	 * Getter do CPF do hóspede.
+	 * @return
+	 * O CPF do hóspede.
+	 */
 	public String getCpf() {
 		return cpf;
 	}
-
+	/**
+	 * Getter da data de nascimento do hóspede.
+	 * @return
+	 * Um objeto Calendar contendo a data de nascimento do hóspede.
+	 */
 	public Calendar getDataNascimento() {
 		return dataNascimento;
 	}
+	/**
+	 * O setter do parâmetro Opiniao do hóspede.
+	 * @param opiniao
+	 * Um objeto Opiniao contendo a opinião do hóspede sobre o hotel.
+	 */
 	public void setOpiniao(Opiniao opiniao){
 		this.opiniao = opiniao;
 	}
+	/**
+	 * O getter do parâmetro opinião do hóspede.
+	 * @return
+	 * Um objeto Opiniao contendo a opinião do hóspede sobre o hotel.
+	 */
 	public Opiniao getOpiniao() {
 		return opiniao;
 	}
 	@Override
 	public String toString(){
-		return "Nome: ";
+		return "Nome: " + getNome() + ".\n"
+				+ "CPF: " + getCpf() + ".\n"
+				+ "Data de Nascimento: " + getDataNascimento() + ".\n" // Ajustar depois a formatação desse objeto Calendar
+				+ ((getOpiniao() == null) ? "." : ("Opinião: " + getOpiniao().toString() + "."));
+
 	}
-	
+
 }
