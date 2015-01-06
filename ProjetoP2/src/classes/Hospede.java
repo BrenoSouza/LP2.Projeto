2d6594eb2d16511454066dfdf9721e780d865a5c
Gabriel Dantas <gabriel.dantas@ccc.ufcg.edu.br>
2015-01-06 19:47:36 -0300

    Excessões

--- ProjetoP2/src/classes/Hospede.java
+++ ProjetoP2/src/classes/Hospede.java
@@ -8,7 +8,10 @@ public class Hospede {
 	private Calendar dataNascimento;
 	private Opiniao opiniao;
 	
-	public Hospede(String nome, String cpf, Calendar dataNascimento){
+	public Hospede(String nome, String cpf, Calendar dataNascimento) throws Exception{
+		if (nome == null|| nome.isEmpty() || cpf == null || cpf.isEmpty() || dataNascimento == null){
+			throw new Exception ("Dados inválidos. Tenta novamente");
+		}
 		this.nome = nome;
 		this.cpf = cpf;
 		this.dataNascimento = dataNascimento;
@@ -34,7 +37,7 @@ public class Hospede {
 	}
 	@Override
 	public String toString(){
-		return "";
+		return "Nome: ";
 	}
 	
 }
