dc1dff0fade533b90b882449c93722690fa9f953
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2015-01-07 00:13:51 -0200

    Métodos da classe serviço!

--- ProjetoP2/src/classes/Servico.java
+++ ProjetoP2/src/classes/Servico.java
@@ -4,20 +4,53 @@ import java.util.Calendar;
 
 public class Servico {
 	
-	Calendar data;
-	Calendar horaEntrada;
-	Calendar horaSaida;
-	int diarias;
+	private Calendar data;
+	private Calendar horaEntrada;
+	private Calendar horaSaida;
+	private int diarias;
 	
 	public Servico(Calendar data, Calendar horaEntrada, Calendar HoraSaida,
-					int diarias) {
+					int diarias, Contrato contrato) throws Exception {
 		
+		// Condição para Exceção
 		
-		
+		this.data = data;
+		this.horaEntrada = horaEntrada;
+		this.horaSaida = horaSaida;
+		this.diarias = diarias;
+	
 	}
 	
-	public Servico(Calendar data, int diarias) {
-		
+	public Calendar getHoraEntrada() {
+		return horaEntrada;	
 	}
 	
+	public Calendar getHoraSaida() {
+		return horaSaida;
+	}
+	
+	public Calendar data() {
+		return data;
+	}
+	
+	public int getNumeroDiarias() {
+		return diarias;
+	}
+	
+	public void setHoraEntrada(Calendar horaEntrada) {
+		this.horaEntrada = horaEntrada;
+	}
+	
+	public void setHoraSaida(Calendar horaSaida) {
+		this.horaSaida = horaSaida;
+	}
+	
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+	public void setDiarias(int diarias) {
+		this.diarias = diarias;
+	}
+
 }
