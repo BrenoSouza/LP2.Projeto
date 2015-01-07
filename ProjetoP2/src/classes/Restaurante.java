38dc9b94d7fa0b2fb850575a90512470a2397fb1
Gabriel Morais <gabmla19@gmail.com>
2015-01-07 18:15:44 -0300

    Javadoc e algumas mudanças em restaurante.

--- ProjetoP2/src/classes/Restaurante.java
+++ ProjetoP2/src/classes/Restaurante.java
@@ -6,20 +6,32 @@ import java.util.Calendar;
 
 		private boolean isCobertura;
 		private double preco;
-		
+		/**
+		 * Construtor da classe restaurante, onde registra o preco da conta.
+		 * @param data Data do uso do servico.
+		 * @param isCobertura Se usou o restaurante da cobertura.
+		 * @param preco Preco da conta
+		 * @throws Exception Caso o preco informado seja menor que zero.
+		 */
 		public Restaurante(Calendar data, boolean isCobertura, double preco) throws Exception {
 			super(data);
+			if (preco < 0){
+				throw new Exception("O preco da conta nao pode ser menor que zero.");
+			}
 			this.isCobertura = isCobertura;
 			this.preco = preco;
 		}
+	/**
+	 * Retorna se é o restaurante da cobertura.
+	 * @return O tipo do restaurante.
+	 */
+	public boolean getIsCobertura() {
+		return isCobertura;
+	}
 	
 	@Override
 	public double calculaPrecoTotal() {
 		return preco;
-	}
-	
-	public boolean getIsCobertura() {
-		return isCobertura;
 	}
 	
 }
