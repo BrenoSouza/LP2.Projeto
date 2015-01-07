58163f3fa3da9a092d67bf84d852c1b49c360217
Gabriel Morais <gabmla19@gmail.com>
2015-01-07 18:14:40 -0300

    Javadoc e algumas mudanças em babysitter.

--- ProjetoP2/src/classes/Babysitter.java
+++ ProjetoP2/src/classes/Babysitter.java
@@ -3,20 +3,24 @@ package classes;
 import java.util.Calendar;
 
 public class Babysitter extends Servico {
-	private Calendar horaEntrada;
 	private Calendar horaSaida;
 	private int horasPrecoNormal;
 	private int horasPrecoExtra;
-	
-	public Babysitter(Calendar horaEntrada, int horaSaida) throws Exception {
+	/**
+	 * Construtor do servico de babysitter.
+	 * @param horaEntrada A hora de entrada da babysitter.
+	 * @param horaSaida A hora de saida da babysitter.
+	 */
+	public Babysitter(Calendar horaEntrada, int horaSaida) {
 		super(horaEntrada);
-		this.horaEntrada = horaEntrada;
 		this.horaSaida = Calendar.getInstance();
 		this.horaSaida.set(Calendar.HOUR_OF_DAY, horaSaida);
 	}
-	
+	/**
+	 * Calcula as horas trabalhadas da babysitter.
+	 */
 	public void horasTrabalhadas(){
-		int inicio = horaEntrada.get(Calendar.HOUR_OF_DAY);
+		int inicio = super.getData().get(Calendar.HOUR_OF_DAY);
 		int fim = horaSaida.get(Calendar.HOUR_OF_DAY);
 		while (inicio != fim){
 			if (inicio < 24){
@@ -31,9 +35,31 @@ public class Babysitter extends Servico {
 			}
 		}
 	}
+	/**
+	 * Retorna a hora de saida da babysitter.
+	 * @return A hora de saida da babysitter.
+	 */
+	public Calendar getHoraSaida() {
+		return horaSaida;
+	}
+	/**
+	 * Retorna as horas trabalhadas em horario normal.
+	 * @return As horas trabalhadas em horario normal.
+	 */
+	public int getHorasPrecoNormal() {
+		return horasPrecoNormal;
+	}
+	/**
+	 * Retorna as horas trabalhadas em horario com preco dobrado.
+	 * @return As horas trabalhadas em horario com preco dobrado.
+	 */
+	public int getHorasPrecoExtra() {
+		return horasPrecoExtra;
+	}
 	
 	@Override
 	public double calculaPrecoTotal() {
+		horasTrabalhadas();
 		return (horasPrecoExtra * 50) + (horasPrecoNormal * 25);
 	}
 
