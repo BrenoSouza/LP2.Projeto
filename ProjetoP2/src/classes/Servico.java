48e204f5e57b4cf2a3399f4cc56f2e650cc9ed0d
Gabriel Morais <gabmla19@gmail.com>
2015-01-07 18:28:50 -0300

    Tirei o calendar do construtor, com isso tirei de todos os outros serviços. Isso porque não fazia sentido ter o parametro Calendar, pois a classe serviço já instancia isso no construtor. Fiz o javadoc de servico tbm.

--- ProjetoP2/src/classes/Servico.java
+++ ProjetoP2/src/classes/Servico.java
@@ -5,16 +5,31 @@ import java.util.Calendar;
 public abstract class Servico {
 	
 	private Calendar data;
-	
-	public Servico(Calendar data) throws Exception {
+	/**
+	 * Construtor de servico
+	 * @param data Data de criacao do servico.
+	 */
+	public Servico() {
 		this.data = Calendar.getInstance();
 	}
-	
+	/**
+	 * Calcula o preco total do servico.
+	 * @return O preco final.
+	 */
 	public abstract double calculaPrecoTotal();
-	
+	/**
+	 * Retorna um Calendar com a data de criacao do servico.
+	 * @return Um Calendar com a data de criacao do servico.
+	 */
 	public Calendar getData() {
 		return data;
 	}
-	
+	/**
+	 * Retorna a hora de entrada do servico.
+	 * @return A hora de entrada do servico.
+	 */
+	public int getHoraEntrada(){
+		return getData().get(Calendar.HOUR_OF_DAY);
+	}
 	
 }
