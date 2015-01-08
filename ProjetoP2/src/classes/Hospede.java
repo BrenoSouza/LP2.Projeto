5725961996682dab869731d7ec8d5e9e09817fa4
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2015-01-08 00:28:53 -0200

    Modificação do toString com a inclusão do endereço. Adição do javadoc dos metodos get e set do parametro endereco.

--- ProjetoP2/src/classes/Hospede.java
+++ ProjetoP2/src/classes/Hospede.java
@@ -7,12 +7,14 @@ public class Hospede {
 	private String nome, cpf;
 	private Calendar dataNascimento;
 	private Opiniao opiniao;
-	private String residencia;
+	private String endereco;
 	
 	/**
 	 * Contrutor da classe Hospede.
 	 * @param nome
 	 * O nome do h�spede.
+	 * @param endereco
+	 * O endereço do Hospede.
 	 * @param cpf
 	 * O CPF do h�spede.
 	 * @param dataNascimento
@@ -20,16 +22,17 @@ public class Hospede {
 	 * @throws Exception
 	 * Joga uma excess�o caso qualquer um dos atributos estiverem em formato inv�lido.
 	 */
-	public Hospede(String nome, String residencia, String cpf, Calendar dataNascimento) throws Exception{
-		if (nome == null || residencia == null || residencia.equals("") || nome.isEmpty() || Hospede.isCpfValido(cpf) == false || dataNascimento == null){
+	public Hospede(String nome, String endereco, String cpf, Calendar dataNascimento) throws Exception{
+		if (nome == null || endereco == null || endereco.isEmpty() || nome.isEmpty() || Hospede.isCpfValido(cpf) == false || dataNascimento == null){
 			throw new Exception ("Dados inv�lidos. Tente novamente");
 		}
 		this.nome = nome;
 		this.cpf = cpf;
 		this.dataNascimento = dataNascimento;
 		this.opiniao = null;
-		this.residencia = residencia;
+		this.endereco = endereco;
 	}
+	
 	/**
 	 * Getter do nome do h�spede.
 	 * @return
@@ -38,6 +41,7 @@ public class Hospede {
 	public String getNome() {
 		return nome;
 	}
+	
 	/**
 	 * Getter do CPF do h�spede.
 	 * @return
@@ -46,6 +50,7 @@ public class Hospede {
 	public String getCpf() {
 		return cpf;
 	}
+	
 	/**
 	 * Getter da data de nascimento do h�spede.
 	 * @return
@@ -54,6 +59,7 @@ public class Hospede {
 	public Calendar getDataNascimento() {
 		return dataNascimento;
 	}
+	
 	/**
 	 * O setter do par�metro Opiniao do h�spede.
 	 * @param opiniao
@@ -62,14 +68,16 @@ public class Hospede {
 	public void setOpiniao(Opiniao opiniao){
 		this.opiniao = opiniao;
 	}
+	
 	/**
 	 * O getter do par�metro opini�o do h�spede.
 	 * @return
-	 * Um objeto Opiniao contendo a opini�o do h�spede sobre o hotel.
+	 * Uma objeto Opiniao contendo a opini�o do h�spede sobre o hotel.
 	 */
 	public Opiniao getOpiniao() {
 		return opiniao;
 	}
+	
 	/**
 	 * M�todo que checa se o CPF inserido � aceit�vel.
 	 * @param cpf
@@ -83,18 +91,30 @@ public class Hospede {
 		}return true;
 	}
 	
-	public void setResidencia(String residencia) {
-		this.residencia = residencia;
+	/**
+	 * O setter do endereco do h�spede.
+	 * @param endereco
+	 * Uma String contendo o endereço do h�spede.
+	 */
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
 	}
 	
-	public String getResidencia() {
-		return residencia;
+	/**
+	 * Getter o endereço do h�spede.
+	 * @return
+	 * Um String contendo o endereço do h�spede.
+	 */
+	public String getEndereco() {
+		return endereco;
 	}
 	
 	@Override
 	public String toString(){
 		return "Nome: " + getNome() + ".\n"
 				+ "CPF: " + getCpf() + ".\n"
+				+ "Endereço: " + getEndereco() + ".\n"
 				+ "Data de Nascimento: " + getDataNascimento() + ".\n"  // Ajustar depois a formata��o desse objeto Calendar
 				+ ((getOpiniao() == null) ? "." : ("Opini�o: " + getOpiniao().toString() + "."));
 
