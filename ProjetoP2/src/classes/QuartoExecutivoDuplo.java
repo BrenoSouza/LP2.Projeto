3a3db5bc65c2d2177d3a85d16f04c2625d08f64a
Gabriel Morais <gabmla19@gmail.com>
2015-01-16 17:11:47 -0300

    JavaDoc de quartos executivo Duplo

--- ProjetoP2/src/classes/QuartoExecutivoDuplo.java
+++ ProjetoP2/src/classes/QuartoExecutivoDuplo.java
@@ -1,7 +1,12 @@
 package classes;
 
 public class QuartoExecutivoDuplo extends Quarto {
-
+	/**
+	 * Construtor de um quarto do tipo Executivo Duplo.
+	 * @param numero Numero do quarto.
+	 * @param diarias Diarias no hotel.
+	 * @throws Exception Caso o numero/numeroHospedes/diarias seja menor que zero.
+	 */
 	public QuartoExecutivoDuplo(int numero, int diarias) throws Exception {
 		super(numero, 3, diarias, 385.0);
 	}
