f96a255f857354f20d890706723223a9328b5ecc
Gabriel Morais <gabmla19@gmail.com>
2015-01-16 17:13:09 -0300

    Javadoc de quarto de luxo duplo.

--- ProjetoP2/src/classes/QuartoLuxoDuplo.java
+++ ProjetoP2/src/classes/QuartoLuxoDuplo.java
@@ -1,7 +1,12 @@
 package classes;
 
 public class QuartoLuxoDuplo extends Quarto {
-
+	/**
+	 * Construtor de um quarto do tipo Luxo Duplo.
+	 * @param numero Numero do quarto.
+	 * @param diarias Diarias no hotel.
+	 * @throws Exception Caso o numero/numeroHospedes/diarias seja menor que zero.
+	 */
 	public QuartoLuxoDuplo(int numero, int diarias) throws Exception {
 		super(numero, 3, diarias, 570.0);
 	}
