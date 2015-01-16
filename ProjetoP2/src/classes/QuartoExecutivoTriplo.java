a67a1cf32418225a2a934d27ba9c6cf0278ba8ec
Gabriel Morais <gabmla19@gmail.com>
2015-01-16 17:12:42 -0300

    JavaDoc de quarto executivo triplo.

--- ProjetoP2/src/classes/QuartoExecutivoTriplo.java
+++ ProjetoP2/src/classes/QuartoExecutivoTriplo.java
@@ -1,7 +1,12 @@
 package classes;
 
 public class QuartoExecutivoTriplo extends Quarto {
-
+	/**
+	 * Construtor de um quarto do tipo Executivo Triplo.
+	 * @param numero Numero do quarto.
+	 * @param diarias Diarias no hotel.
+	 * @throws Exception Caso o numero/numeroHospedes/diarias seja menor que zero.
+	 */
 	public QuartoExecutivoTriplo(int numero, int diarias) throws Exception {
 		super(numero, 3, diarias, 440.0);
 	}
