6b2379b03ca8bfbdec86b90d87c66370f2251844
Gabriel <Gabriel@Gabriel-Note>
2015-01-18 20:53:28 -0300

    mudanças

--- ProjetoP2/src/classes/Restaurante.java
+++ ProjetoP2/src/classes/Restaurante.java
@@ -1,37 +1,37 @@
 package classes;
 
-	public class Restaurante extends Servico {
+public class Restaurante extends Servico {
 
-		private boolean isCobertura;
-		private double preco;
-		/**
-		 * Construtor da classe restaurante, onde registra o preco da conta.
-		 * @param data Data do uso do servico.
-		 * @param isCobertura Se usou o restaurante da cobertura.
-		 * @param preco Preco da conta
-		 * @throws Exception Caso o preco informado seja menor que zero.
-		 */
-		public Restaurante(boolean isCobertura, double preco) throws Exception {
-			super();
-			if (preco < 0){
-				throw new Exception("O preco da conta nao pode ser menor que zero.");
-			}
-			this.isCobertura = isCobertura;
-			this.preco = preco;
-		}
+	private boolean isCobertura;
+	private double preco;
 	/**
-	 * Retorna se � o restaurante da cobertura.
+	 * Construtor da classe restaurante, onde registra o preco da conta.
+	 * @param data Data do uso do servico.
+	 * @param isCobertura Se usou o restaurante da cobertura.
+	 * @param preco Preco da conta
+	 * @throws Exception Caso o preco informado seja menor que zero.
+	 */
+	public Restaurante(boolean isCobertura, double preco) throws Exception {
+		super();
+		if (preco < 0){
+			throw new Exception("O preco da conta nao pode ser menor que zero.");
+		}
+		this.isCobertura = isCobertura;
+		this.preco = preco;
+	}
+	/**
+	 * Retorna se o restaurante é da cobertura ou não.
 	 * @return O tipo do restaurante.
 	 */
 	public boolean getIsCobertura() {
 		return isCobertura;
 	}
-	
+
 	@Override
 	public double calculaPrecoTotal() {
 		return preco;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Servico --- Restaurante ---" +
@@ -42,5 +42,5 @@ package classes;
 	public String getTipo() {
 		return "Restaurante" + (getIsCobertura() ? "Cobertura" : "");
 	}
-	
+
 }
--- ProjetoP2/src/gui/PainelVisualizacaoServico.java
+++ ProjetoP2/src/gui/PainelVisualizacaoServico.java
@@ -21,12 +21,12 @@ public class PainelVisualizacaoServico extends JInternalFrame {
 		setClosable(true);
 		setBounds(100, 100, 253, 302);
 		
-		JLabel lblDeclaração = new JLabel("Declara\u00E7\u00E3o:");
-		lblDeclaração.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		JLabel lblDeclaracao = new JLabel("Declara\u00E7\u00E3o:");
+		lblDeclaracao.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		
 		JLabel lblToStringdoServico = new JLabel("New label");
 		lblToStringdoServico.setText("<html><br>" + servico.toString() + "</br></html>");
-		//Pequeno problema. OJlabel não reconhece as tags /n para quebrar uma linha. Um jeito é vc cercar a string com essas tags html ali em cima, mas elas só quebram quando chega no final da janela.
+		//Pequeno problema. OJlabel nï¿½o reconhece as tags /n para quebrar uma linha. Um jeito ï¿½ vc cercar a string com essas tags html ali em cima, mas elas sï¿½ quebram quando chega no final da janela.
 		lblToStringdoServico.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
@@ -34,7 +34,7 @@ public class PainelVisualizacaoServico extends JInternalFrame {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblDeclaração)
+						.addComponent(lblDeclaracao)
 						.addComponent(lblToStringdoServico))
 					.addContainerGap(544, Short.MAX_VALUE))
 		);
@@ -42,7 +42,7 @@ public class PainelVisualizacaoServico extends JInternalFrame {
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(lblDeclaração)
+					.addComponent(lblDeclaracao)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(lblToStringdoServico)
 					.addContainerGap(223, Short.MAX_VALUE))
