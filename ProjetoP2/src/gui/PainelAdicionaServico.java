0baaf8de4c574de36436181a7a4d7e4f42ddcb72
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2015-01-19 07:11:15 -0200

    Botoes do PainelAdicionaServico!

--- ProjetoP2/src/gui/PainelAdicionaServico.java
+++ ProjetoP2/src/gui/PainelAdicionaServico.java
@@ -6,8 +6,16 @@ import javax.swing.JDesktopPane;
 import javax.swing.JInternalFrame;
 
 import classes.Contrato;
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class PainelAdicionaServico extends JInternalFrame {
 
@@ -18,18 +26,46 @@ public class PainelAdicionaServico extends JInternalFrame {
 		setClosable(true);
 		setBounds(100, 100, 800, 400);
 		
+		Icon imagemQuarto = new ImageIcon(PainelServicos.class.getResource("/resources/quarto.png"));
+		JButton btnQuartos = new JButton(imagemQuarto);
+		
+		Icon imagemCarro = new ImageIcon(PainelServicos.class.getResource("/resources/carro.png"));
+		JButton btnAluguelCarros = new JButton(imagemCarro);
+		
+		Icon imagemBabysitter = new ImageIcon(PainelServicos.class.getResource("/resources/quarto.png"));
+		JButton btnBabysitter = new JButton(imagemBabysitter);
+		
+		Icon imagemRestaurante = new ImageIcon(PainelServicos.class.getResource("/resources/restaurante.png"));
+		JButton btnRestaurante = new JButton(imagemRestaurante);
+
 		
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 790, Short.MAX_VALUE)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addGap(152)
+					.addComponent(btnQuartos, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+					.addGap(54)
+					.addComponent(btnAluguelCarros, 0, 0, Short.MAX_VALUE)
+					.addGap(57)
+					.addComponent(btnBabysitter, 0, 0, Short.MAX_VALUE)
+					.addGap(78)
+					.addComponent(btnRestaurante, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
+					.addGap(131))
 		);
 		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 368, Short.MAX_VALUE)
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addGap(43)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+							.addComponent(btnAluguelCarros, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(btnBabysitter, GroupLayout.DEFAULT_SIZE, 74, Short.MAX_VALUE)
+							.addComponent(btnRestaurante, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE))
+						.addComponent(btnQuartos, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addContainerGap(251, Short.MAX_VALUE))
 		);
 		getContentPane().setLayout(groupLayout);
 
 	}
-
 }
