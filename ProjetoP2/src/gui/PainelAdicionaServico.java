9561a2f06b29395189a0f82ae630e42c65949596
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2015-01-19 05:39:04 -0200

    Edição do PainelAdicionaServico!

--- ProjetoP2/src/gui/PainelAdicionaServico.java
+++ ProjetoP2/src/gui/PainelAdicionaServico.java
@@ -2,15 +2,33 @@ package gui;
 
 import java.awt.EventQueue;
 
+import javax.swing.JDesktopPane;
 import javax.swing.JInternalFrame;
+
+import classes.Contrato;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class PainelAdicionaServico extends JInternalFrame {
 
 	/**
 	 * Create the frame.
 	 */
-	public PainelAdicionaServico() {
-		setBounds(100, 100, 450, 300);
+	public PainelAdicionaServico(Contrato contrato, JDesktopPane painelPrincipal) {
+		setClosable(true);
+		setBounds(100, 100, 800, 400);
+		
+		
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 790, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 368, Short.MAX_VALUE)
+		);
+		getContentPane().setLayout(groupLayout);
 
 	}
 
