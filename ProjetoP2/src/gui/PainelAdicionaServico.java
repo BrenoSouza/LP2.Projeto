0781aab9e4040f8352900f971ceefc31970eb5d1
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2015-01-19 04:44:26 -0200

    Criando a classe PainelAdicionaServico!

---
+++ ProjetoP2/src/gui/PainelAdicionaServico.java
@@ -0,0 +1,17 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class PainelAdicionaServico extends JInternalFrame {
+
+	/**
+	 * Create the frame.
+	 */
+	public PainelAdicionaServico() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
