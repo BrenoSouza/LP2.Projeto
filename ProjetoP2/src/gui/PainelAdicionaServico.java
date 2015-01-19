1cb2805868840810caba721fa6d636928725e943
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2015-01-19 07:42:03 -0200

    Outras modificações nos Botões!

--- ProjetoP2/src/gui/PainelAdicionaServico.java
+++ ProjetoP2/src/gui/PainelAdicionaServico.java
@@ -32,38 +32,58 @@ public class PainelAdicionaServico extends JInternalFrame {
 		Icon imagemCarro = new ImageIcon(PainelServicos.class.getResource("/resources/carro.png"));
 		JButton btnAluguelCarros = new JButton(imagemCarro);
 		
-		Icon imagemBabysitter = new ImageIcon(PainelServicos.class.getResource("/resources/quarto.png"));
+		Icon imagemBabysitter = new ImageIcon(PainelServicos.class.getResource("/resources/babysitter.png"));
 		JButton btnBabysitter = new JButton(imagemBabysitter);
 		
 		Icon imagemRestaurante = new ImageIcon(PainelServicos.class.getResource("/resources/restaurante.png"));
 		JButton btnRestaurante = new JButton(imagemRestaurante);
+		
+		JButton btnAdicionar = new JButton("Adicionar");
+		
+		JButton btnCancelar = new JButton("Cancelar");
 
 		
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addGap(152)
-					.addComponent(btnQuartos, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
-					.addGap(54)
-					.addComponent(btnAluguelCarros, 0, 0, Short.MAX_VALUE)
-					.addGap(57)
-					.addComponent(btnBabysitter, 0, 0, Short.MAX_VALUE)
-					.addGap(78)
-					.addComponent(btnRestaurante, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
-					.addGap(131))
-		);
-		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(btnAdicionar))
+						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+							.addGap(152)
+							.addComponent(btnQuartos, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+							.addGap(54)
+							.addComponent(btnAluguelCarros, 0, 0, Short.MAX_VALUE)))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(57)
+							.addComponent(btnBabysitter, 0, 0, Short.MAX_VALUE)
+							.addGap(65)
+							.addComponent(btnRestaurante, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
+							.addGap(144))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(18)
+							.addComponent(btnCancelar)
+							.addContainerGap())))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(43)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-							.addComponent(btnAluguelCarros, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(btnBabysitter, GroupLayout.DEFAULT_SIZE, 74, Short.MAX_VALUE)
-							.addComponent(btnRestaurante, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE))
-						.addComponent(btnQuartos, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-					.addContainerGap(251, Short.MAX_VALUE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnRestaurante, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnAluguelCarros, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(btnBabysitter, GroupLayout.DEFAULT_SIZE, 74, Short.MAX_VALUE))
+							.addComponent(btnQuartos, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+					.addPreferredGap(ComponentPlacement.RELATED, 205, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnAdicionar)
+						.addComponent(btnCancelar))
+					.addGap(21))
 		);
 		getContentPane().setLayout(groupLayout);
 
