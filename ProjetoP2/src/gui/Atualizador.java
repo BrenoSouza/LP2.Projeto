f1fb5230e64b45f7fbd2bfb423df8fb9f06b917e
Gabriel Dantas <gabriel.dantas@ccc.ufcg.edu.br>
2015-03-11 15:47:48 -0300

    Comments de Atualizador

--- ProjetoP2/src/gui/Atualizador.java
+++ ProjetoP2/src/gui/Atualizador.java
@@ -2,6 +2,14 @@ package gui;
 
 public interface Atualizador {
 
+  /*
+   * Interface feita pra se usar com JInternalFrames que abrem outros JInternalFrames.
+   * Quando determinada ação acontece, o JInternalFrame filho chamará a função atualiza do pai, que lidará do seu próprio jeito.
+   */
+  
+  /**
+   * Método que atualiza a classe chamada, cada uma de seu jeito.
+   */
   public void atualiza();
   
   
